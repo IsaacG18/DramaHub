4d795739d06e6a8ed22abe34a6819554b9be3d36
Isaac Gilbert <1415244@dundeeandangus.ac.uk>
2021-05-05 18:49:05 +0100

    commit all

--- dbconnection.php
+++ dbconnection.php
@@ -1,18 +1,22 @@
 <?php
 try{
-             $host = 'localhost';
-             $dbname = 'dramahub';
-             $username = 'root';
-             $password = '';
+            $host = 'studentnet.dundeeandangus.ac.uk';
+            $dbname =   'db_1415244';
+            $username = '1415244';
+            $password = 'Password1!';
+//             $host = 'localhost';//Host name for database
+//             $dbname = 'dramahub';//Database name
+//             $username = 'root';//Username for the database
+//             $password = '';//Password for the database
 
  
-          $con = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8',$username,$password);
+          $con = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8',$username,$password);//Attempts to connect to database
 
-
+          $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     }catch(PDOException $e){
-        echo $e->getMessage();
-          die("Connection Failed");
+        echo $e->getMessage();//If this data is sent fails
+        die("Connection Failed");
     }
     
 ?>
